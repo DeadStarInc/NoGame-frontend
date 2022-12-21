ba1c0414e6e46d63bf16ea1ee5a61156f67421c8
Alex Metelli <alex-metelli@gmx.com>
2022-12-21 07:08:53 +0000

    [chore] refactored for latest starknet libraries

--- src/hooks/calls/useUpgradeResourceComplete.tsx
+++ src/hooks/calls/useUpgradeResourceComplete.tsx
@@ -1,67 +1,69 @@
-import { useStarknet } from '@starknet-react/core'
-import { useCallback } from 'react'
-import { AddTransactionResponse } from 'starknet'
-import { useS2MTransactionManager } from '~/providers/transaction'
-import { useGameContract } from '../game'
+import { useStarknet } from "@starknet-react/core";
+import { useCallback } from "react";
+import { Sequencer } from "starknet";
+import { useS2MTransactionManager } from "~/providers/transaction";
+import { useGameContract } from "../game";
 
 export type ResourceType =
-  | 'metal'
-  | 'crystal'
-  | 'deuterium'
-  | 'solar'
-  | 'robot'
-  | 'shipyard'
-  | 'research'
-  | 'nanite'
-  | 'cargoShip'
-  | 'recyclerShip'
-  | 'espionageProbe'
-  | 'solarSatellite'
-  | 'lightFighter'
-  | 'cruiserBuild'
-  | 'battleShip'
-  | 'armourTech'
-  | 'astrophysics'
-  | 'combustionDrive'
-  | 'computerTech'
-  | 'energyTech'
-  | 'espionageTech'
-  | 'hyperspaceDrive'
-  | 'hyperspaceTech'
-  | 'impulseDrive'
-  | 'ionTech'
-  | 'laserTech'
-  | 'plasmaTech'
-  | 'shieldingTech'
-  | 'weaponsTech'
+    | "metal"
+    | "crystal"
+    | "deuterium"
+    | "solar"
+    | "robot"
+    | "shipyard"
+    | "research"
+    | "nanite"
+    | "cargoShip"
+    | "recyclerShip"
+    | "espionageProbe"
+    | "solarSatellite"
+    | "lightFighter"
+    | "cruiserBuild"
+    | "battleShip"
+    | "armourTech"
+    | "astrophysics"
+    | "combustionDrive"
+    | "computerTech"
+    | "energyTech"
+    | "espionageTech"
+    | "hyperspaceDrive"
+    | "hyperspaceTech"
+    | "impulseDrive"
+    | "ionTech"
+    | "laserTech"
+    | "plasmaTech"
+    | "shieldingTech"
+    | "weaponsTech";
 
 export default function useCompleteResource(resourceName: ResourceType) {
-  const { account } = useStarknet()
-  const { contract } = useGameContract()
+    const { account } = useStarknet();
+    const { contract } = useGameContract();
 
-  const { addTransaction } = useS2MTransactionManager()
+    const { addTransaction } = useS2MTransactionManager();
 
-  return useCallback(async () => {
-    if (!contract || !account) {
-      throw new Error('Missing Dependencies')
-    }
+    return useCallback(async () => {
+        if (!contract || !account) {
+            throw new Error("Missing Dependencies");
+        }
 
-    return contract
-      .invoke(`${resourceName}UpgradeComplete`, [])
-      .then((tx: AddTransactionResponse) => {
-        console.log('Transaction hash: ', tx.transaction_hash)
+        return contract
+            .invoke(`${resourceName}UpgradeComplete`, [])
+            .then((tx: Sequencer.AddTransactionResponse) => {
+                console.log("Transaction hash: ", tx.transaction_hash);
 
-        addTransaction({
-          status: tx.code,
-          transactionHash: tx.transaction_hash,
-          address: account,
-          summary: `Complete ${resourceName}`,
-        })
+                addTransaction({
+                    code: tx.code!,
+                    status: "NOT_RECEIVED",
+                    transactionHash: tx.transaction_hash,
+                    address: account,
+                    lastUpdatedAt: 0,
+                    summary: `Complete ${resourceName}`,
+                });
 
-        return tx.transaction_hash
-      })
-      .catch((e) => {
-        console.error(e)
-      })
-  }, [account, addTransaction, contract])
+                return tx.transaction_hash;
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    }, [account, addTransaction, contract]);
 }
