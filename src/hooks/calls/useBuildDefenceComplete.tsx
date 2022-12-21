ec51c2a0d37738a113651d906ebd65dfb30181f6
Alex Metelli <alex-metelli@gmx.com>
2022-12-21 09:18:58 +0000

    [defences] added call for upgrade start

---
+++ src/hooks/calls/useBuildDefenceComplete.tsx
@@ -0,0 +1,49 @@
+import { useAccount } from "@starknet-react/core";
+import { useCallback } from "react";
+import { Sequencer } from "starknet";
+import { useS2MTransactionManager } from "~/providers/transaction";
+import { useGameContract } from "../game";
+
+export type DefenceType =
+    | "rocket"
+    | "lightLaser"
+    | "heavyLaser"
+    | "ionCannon"
+    | "gauss"
+    | "plasmaTurret"
+    | "smallDome"
+    | "largeDome";
+
+export default function useBuildShipComplete(resourceName: DefenceType) {
+    // const { account } = useStarknet();
+    const { address } = useAccount();
+    const { contract } = useGameContract();
+
+    const { addTransaction } = useS2MTransactionManager();
+
+    return useCallback(async () => {
+        if (!contract || !address) {
+            throw new Error("Missing Dependencies");
+        }
+
+        return contract
+            .invoke(`${resourceName}BuildComplete`, [])
+            .then((tx: Sequencer.AddTransactionResponse) => {
+                console.log("Transaction hash: ", tx.transaction_hash);
+
+                addTransaction({
+                    status: "NOT_RECEIVED",
+                    code: tx.code!,
+                    transactionHash: tx.transaction_hash,
+                    address: address,
+                    lastUpdatedAt: 0,
+                    summary: `Complete ${resourceName}`,
+                });
+
+                return tx.transaction_hash;
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    }, [address, addTransaction, contract]);
+}
